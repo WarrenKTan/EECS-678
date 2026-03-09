420fb82162f37b9e0e3aa03e2ef3b39f59e74992
WarrenKTan <143449702+WarrenKTan@users.noreply.github.com>
2026-03-09 18:38:06 -0500

    Lab 4 Submission

--- Lab4/pipe-sync.c
+++ Lab4/pipe-sync.c
@@ -23,19 +23,38 @@ int main()
   s  = "Use Pipe for Process Synchronization\n";
 
   /* create pipe */
+  int p1[2], p2[2];
+  pipe(p1);
+  pipe(p2);
 
   ret = fork();
   if (ret == 0) {
-
     /* child process. */
+    close(p1[0]);
+    close(p2[1]);
+
     printf("Child line 1\n");
+    
+    // tell parent to continue execution
+    write(p1[1], "_", 1);
+    
+    // wait for parent to send signal
+    read(p2[0], &buf, 1);
     printf("Child line 2\n");
   } else {
-
     /* parent process */
+    close(p1[1]);
+    close(p2[0]);
+    
+    // wait for child to send signal
+    read(p1[0], &buf, 1);
     printf("Parent line 1\n");
-    printf("Parent line 2\n");
+    
+    // tell child to continue execution
+    write(p2[1], "_", 1);
 
+    // wait for child to finish execution
     wait(&stat);
+    printf("Parent line 2\n");
   }
 }
