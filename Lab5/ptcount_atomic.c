89bf5a71832bc35dba000c986489f25a296b5593
WarrenKTan <143449702+WarrenKTan@users.noreply.github.com>
2026-03-23 00:41:06 -0500

    Lab5 Submission

--- Lab5/ptcount_atomic.c
+++ Lab5/ptcount_atomic.c
@@ -31,7 +31,7 @@ void *inc_count(void *arg)
      * does their repsective locations have for critical section
      * existence and the need for Critical section protection?
      */
-    count = count + my_args->inc;
+    __atomic_add_fetch(&count, my_args->inc, __ATOMIC_RELAXED);
     loc = loc + my_args->inc;
   }
   printf("Thread: %d finished. Counted: %d\n", my_args->tid, loc);
@@ -77,6 +77,7 @@ int main(int argc, char *argv[])
     targs->loop = loop;
     targs->inc = inc;
     /* Make call to pthread_create here */
+    pthread_create(&threads[i], &attr, inc_count, (void *)targs);
   }
 
   /* Wait for all threads to complete using pthread_join.  The threads
@@ -84,6 +85,7 @@ int main(int argc, char *argv[])
    */
   for (i = 0; i < NUM_THREADS; i++) {
     /* Make call to pthread_join here */
+    pthread_join(threads[i], NULL);
   }
 
   printf ("Main(): Waited on %d threads. Final value of count = %d. Done.\n",
--- Lab5/ptcount_mutex.c
+++ Lab5/ptcount_mutex.c
@@ -31,7 +31,9 @@ void *inc_count(void *arg)
      * does their repsective locations have for critical section
      * existence and the need for Critical section protection?
      */
+    pthread_mutex_lock(&count_mutex);
     count = count + my_args->inc;
+    pthread_mutex_unlock(&count_mutex);
     loc = loc + my_args->inc;
   }
   printf("Thread: %d finished. Counted: %d\n", my_args->tid, loc);
@@ -77,6 +79,7 @@ int main(int argc, char *argv[])
     targs->loop = loop;
     targs->inc = inc;
     /* Make call to pthread_create here */
+    pthread_create(&threads[i], &attr, inc_count, (void *)targs);
   }
 
   /* Wait for all threads to complete using pthread_join.  The threads
@@ -84,6 +87,7 @@ int main(int argc, char *argv[])
    */
   for (i = 0; i < NUM_THREADS; i++) {
     /* Make call to pthread_join here */
+    pthread_join(threads[i], NULL);
   }
 
   printf ("Main(): Waited on %d threads. Final value of count = %d. Done.\n",
